5a7c951db788c7866d5ae47a8fabdd8798639209
Sridhar Rajaram <srilumia@hotmail.com>
2023-10-04 13:12:28 +0530

    register route updated

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const mysql = require("mysql");
+const PORT = 5001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port:3306,
+    user: 'root',
+    password: 'Mysql@123$',
+    database: 'calsync'
+})
+
+app.post('/register', (req,res) => {
+        var sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (?)";
+        //Make an array of values:
+        var values = [
+          req.body.name,
+          req.body.email,
+          req.body.password
+        ];
+        //Execute the SQL statement, with the value array:
+        db.query(sql, [values], function (err, data) {
+          if (err){
+            return res.json("Error DB");
+          }
+          return res.json(data);
+        });
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server started running on port ${PORT}`)
+})
