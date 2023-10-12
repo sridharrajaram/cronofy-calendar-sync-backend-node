e0e4b7953a59332b2750c5c460dbdb9855bd442e
Sridhar Rajaram <srilumia@hotmail.com>
2023-10-12 12:36:59 +0530

    logout route module updated

--- index.js
+++ index.js
@@ -104,6 +104,12 @@ app.post("/login", (req, res) => {
   })
 });
 
+
+app.get('/logout', (req,res)=>{
+res.clearCookie('authCookie');
+return res.json({status:"Success"});
+})
+
 app.listen(PORT, () => {
   console.log(`Server started running on port ${PORT}`);
 });
